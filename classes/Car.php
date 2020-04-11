c4325cd60f372a7c38c42df089926567cbf30869
Vladiid <vladiid84@gmail.com>
2020-04-11 17:48:24 +0300

    Update Car.php

--- classes/Car.php
+++ classes/Car.php
@@ -1,6 +1,20 @@
 <?php
-class Car{
+
+class Car
+{
     public $color;
-    public $speed = 180;
+    public $speed;
     public $brand;
+    public $wheels = 4;
+
+    public function getCarInfo()
+    {
+        return "<h3>About auto:</h3>
+                Brand: {$this->brand}<br>
+                Color: {$this->color} <br>
+                Speed: {$this->speed} <br>
+                Wheels: {$this->wheels} <br>
+                Year: {$this->year} <br>
+                ";
+    }
 }
