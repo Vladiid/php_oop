cccff6f24e516f9bf0e1dae019cd424f8b911979
Vladiid <vladiid84@gmail.com>
2020-04-17 17:30:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,6 @@ $car1 = new Car('Black', 220, 'Honda', 4, 2020);
 $car2 = new Car('White', 250, 'Tesla', 4, 2020);
 $car3 = new Car('Red', 180, 'Audi', 4, 2020);
 
-echo $car1  ->getCarInfo();
-echo $car2  ->getCarInfo();
-echo $car3  ->getCarInfo();
+echo $car1->getCarInfo();
+echo $car2->getCarInfo();
+echo $car3->getCarInfo();
