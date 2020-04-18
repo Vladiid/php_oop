e673b6c7971ee8d0994613c1a41bf7258d300883
Vladiid <vladiid84@gmail.com>
2020-04-18 16:49:12 +0300

    Update index.php

--- 1/index.php
+++ 1/index.php
@@ -1,7 +1,17 @@
 <?php
-require_once 'classes/File.php';
 
-$file = new File(__DIR__ . '/file.txt');
-$file->write('line 1');
-$file->write('line 2');
-$file->write('line 3');
+//require_once 'classes/File.php';    // путь к файлу с классом
+//$file = new file(__DIR__ . '/file.txt');    //файл в который происходит запись
+//
+//$file ->write("line 1");
+//$file ->write("line 2");    //вызов финкции запись
+//$file ->write("line 3");
+//$file ->write("line 4");
+
+require_once 'classes/File.php';
+$fpath = new File(__DIR__. '/file.txt');
+
+$fpath->write('text1');
+$fpath->write('text1');
+$fpath->write('text1');
+$fpath->write('text1');
