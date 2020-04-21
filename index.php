61b0e65681640dfc5e61ac607a0a00899937e665
Vladiid <vladiid84@gmail.com>
2020-04-21 03:25:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,18 @@ function debug($data)
     echo '<pre>' . print_r($data, 1) . '</pre>';
 }
 
+echo Car::$countCar;
+echo "<br>";
 $car1 = new Car('Black', 220, 'Honda', 4, 2020);
+echo Car::$countCar;
+echo "<br>";
 $car2 = new Car('White', 250, 'Tesla', 4, 2020);
+echo Car::$countCar;
+echo "<br>";
 $car3 = new Car('Red', 180, 'Audi', 4, 2020);
+echo Car::getCount();
 
 echo $car1->getCarInfo();
 echo $car2->getCarInfo();
 echo $car3->getCarInfo();
+echo $car1->getPrototypeInfo();
