2c55b565837fc13878819fed6fe3003f6a5b0e8d
Vladiid <vladiid84@gmail.com>
2020-05-09 15:39:05 +0300

    Create Product.php

---
+++ 2/classes/Product.php
@@ -0,0 +1,45 @@
+<?php
+
+class Product
+{
+    public $name;
+    public $price;
+
+
+    public $cpu;
+    public $numPages;
+
+    public function __construct($name, $price, $cpu = null, $numPages = null)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->cpu = $cpu;
+        $this->numPages = $numPages;
+
+    }
+
+    public function getCpu()
+    {
+        return $this->cpu;
+    }
+
+
+    public function getNumPages()
+    {
+        return $this->numPages;
+    }
+
+    public function getProduct($type = 'notebook')
+    {
+        $out = "<hr><b>About Product:</b><br>
+                Name: {$this->name}<br>
+                Price: {$this->price}<br>";
+        if($type == 'notebook'){
+            $out .= "CPU: {$this->cpu}<br>";
+        }else{
+            $out .= "Number of pages: {$this->numPages}<br>";
+        }
+        return $out;
+
+    }
+}
