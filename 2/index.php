9dbdb63a910bfb7ffdbdca79fc82748cb052a285
Vladiid <vladiid84@gmail.com>
2020-05-30 14:51:34 +0300

    Update index.php

--- 2/index.php
+++ 2/index.php
@@ -1,18 +1,20 @@
 <?php
 
 require_once 'classes/Product.php';
+require_once 'classes/NotebookProduct.php';
+require_once 'classes/BookProduct.php';
 
 function debug($data)
 {
     echo '<pre>' . print_r($data, 1) . '</pre>';
 }
 
-$book = new Product('Shine', 25, null, 370);
+$book = new BookProduct('Shine', 25, 477);
 
-$notebook = new Product('Macbook pro', 1799, 'Intel Core I9');
+$notebook = new NotebookProduct('Macbook pro', 1799, 'intel');
 
-//debug($book);
-//debug($notebook);
+debug($book);
+debug($notebook);
 
-echo $book->getProduct('book');
-echo $notebook->getProduct('notebook');
+echo $book->getProduct();
+echo $notebook->getProduct();
