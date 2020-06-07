a622d9f13d10592602331afe305dc56bc04ea11b
Vladiid <vladiid84@gmail.com>
2020-06-07 19:01:55 +0300

    Update index.php

--- 2/index.php
+++ 2/index.php
@@ -2,20 +2,26 @@
 
 require_once 'classes/Product.php';
 require_once 'classes/NotebookProduct.php';
+require_once 'classes/IGadget.php';
 require_once 'classes/BookProduct.php';
+
 
 function debug($data)
 {
     echo '<pre>' . print_r($data, 1) . '</pre>';
 }
 
+function offerCase(IGadget $product){
+    echo "<p>Laptop bag as a gift for {$product->getName()}!</p>";
+}
+
+var_dump($book instanceof IGadget);
+var_dump($notebook instanceof NotebookProduct);
+
 $book = new BookProduct('Shine', 25, 20);
+$notebook = new NotebookProduct('Macbook pro', 2900, 'intel');
 
-$notebook = new NotebookProduct('Macbook pro', 100, 'intel');
 
+//offerCase($book);
 
-debug($book);
-debug($notebook);
-
-echo $book->getProduct();
-echo $notebook->getProduct();
+//offerCase($notebook);
