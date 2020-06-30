81300ae3d3c07413fa49efc1035e4b11827c8920
Vladiid <vladiid84@gmail.com>
2020-06-30 07:32:08 +0300

    Update index.php

--- 2/index.php
+++ 2/index.php
@@ -1,37 +1,46 @@
 <?php
 
-//require_once 'classes/Product.php';
-//require_once 'classes/IGadget.php';
-//require_once 'classes/NotebookProduct.php';
-//require_once 'classes/BookProduct.php';
+use classes\BookProduct;
+use classes\interfaces\IGadget;
+use classes\NotebookProduct;
 
-function autoloder($class)
+function auto_loader1($class)
 {
-    $file = __DIR__ . "/classes/{$class}.php";
-    if(file_exists($file)){
+    $class = str_replace("\\", '/', $class);
+    $file = __DIR__ . "/{$class}.php";
+    if (file_exists($file)) {
         require_once $file;
     }
 }
 
-spl_autoload_register('autoloder');
+//function auto_loader2($class){
+//    $file = __DIR__."/classes/interfaces/{$class}.php";
+//    if(file_exists($file)){
+//        require_once $file;
+//    }
+//}
+
+spl_autoload_register('auto_loader1');
+//spl_autoload_register('auto_loader2');
+
 
 function debug($data)
 {
     echo '<pre>' . print_r($data, 1) . '</pre>';
 }
 
-function offerCase(IGadget $product){
+function offerCase(IGadget $product)
+{
     echo "<p>Laptop bag as a gift for {$product->getName()}!</p>";
 }
 
 $notebook = new NotebookProduct('Macbook pro', 3000, 'intel');
 
 
-
 $book = new BookProduct('Shine', 25, 20);
 $notebook = new NotebookProduct('Macbook pro', 2900, 'intel');
 
-//offerCase($book);
+
 offerCase($notebook);
 
 echo $notebook->getProduct();
