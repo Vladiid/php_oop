51ff5a3a8f93e4627df21d070d0b67b9510d6161
Vladiid <vladiid84@gmail.com>
2020-07-10 16:23:11 +0300

    Create BookProduct.php

---
+++ 2/app/BookProduct.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app;
+
+use wfm\Product;
+
+class BookProduct extends Product
+{
+    public $pages;
+
+    public function __construct($name, $price, $pages)
+    {
+        parent::__construct($name, $price);
+        $this->pages = $pages;
+    }
+
+    public function getProduct()
+    {
+        $out = parent::getProduct();
+        $out .= "Pages: {$this->pages}<br>";
+        return $out;
+
+    }
+
+    public function getPages()
+    {
+        return $this->pages;
+
+    }
+
+
+}
